54315768af37bc26f466f4caf69e1e43ee6085d3
tingsha <sansword@mail.ru>
2021-09-28 00:50:37 +0500

    select text color in statistic window

--- src/main/java/view/SettingsView.java
+++ src/main/java/view/SettingsView.java
@@ -6,6 +6,9 @@ import org.slf4j.LoggerFactory;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.plaf.synth.SynthLookAndFeel;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledDocument;
@@ -22,6 +25,7 @@ public class SettingsView extends JWindow implements View {
     private Controller controller;
     private static final Logger logger = LoggerFactory.getLogger(SettingsView.class);
     private JFrame mainView;
+    private JTextPane preview = getPreview();
 
     public SettingsView(JFrame mainView) {
         this.mainView = mainView;
@@ -46,17 +50,28 @@ public class SettingsView extends JWindow implements View {
         constraints.gridx = 0;
         constraints.gridy = 1;
         constraints.gridwidth = 2;
-        rootPanel.add(getPreview(), constraints);
+        rootPanel.add(preview, constraints);
 
-        constraints.insets = new Insets(5, 0, 30, 0);
         constraints.gridx = 0;
         constraints.gridy = 2;
+        rootPanel.add(createSeparatedLine(), constraints);
+
+        constraints.insets = new Insets(10, 0, 30, 0);
+        constraints.gridx = 0;
+        constraints.gridy = 3;
         constraints.gridwidth = 1;
-        rootPanel.add(createColorTitle(), constraints);
+        rootPanel.add(createJTextField("Color"), constraints);
 
         constraints.insets = new Insets(0, 0, 30, 0);
         constraints.gridx = 1;
         rootPanel.add(createColorsPanel(), constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 4;
+        rootPanel.add(createJTextField("Alpha"), constraints);
+
+        constraints.gridx = 1;
+        rootPanel.add(createSlider(), constraints);
 
         rootPanel.setPreferredSize(new Dimension(350, 400));
         add(rootPanel);
@@ -68,13 +83,49 @@ public class SettingsView extends JWindow implements View {
         pack();
     }
 
+    private JSlider createSlider(){
+        try {
+            SynthLookAndFeel laf = new SynthLookAndFeel();
+            laf.load(SettingsView.class.getResourceAsStream("slider/slider.xml"), SettingsView.class);
+            UIManager.setLookAndFeel(laf);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        JSlider slider = new JSlider(0, 255);
+        slider.setBackground(Colors.TOOLBAR_BACKGROUND);
+        slider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                Color rgb = new Color(preview.getForeground().getRGB());
+                preview.setForeground(new Color(rgb.getRed(), rgb.getGreen(), rgb.getBlue(),
+                        ((JSlider) e.getSource()).getValue()));
+            }
+        });
+        return slider;
+    }
+
+    private JPanel createSeparatedLine(){
+        JPanel panel = new JPanel(){
+            @Override
+            protected void paintComponent(Graphics g) {
+                Graphics2D g2d = (Graphics2D) g;
+                g2d.setStroke(new BasicStroke(3));
+                g2d.setColor(Colors.DESELECTED_LINE);
+                g2d.drawLine(0, 0,300, 0);
+            }
+        };
+        panel.setPreferredSize(new Dimension(300, 3));
+        panel.setBackground(Color.RED);
+        return panel;
+    }
+
     private JPanel createColorsPanel(){
         JPanel panel = new JPanel();
         panel.setBackground(Colors.TOOLBAR_BACKGROUND);
         FlowLayout flowLayout = new FlowLayout();
         flowLayout.setHgap(10);
         panel.setLayout(flowLayout);
-        String[] paths = {"src/main/resources/img/settings/redCircle.png",
+        String[] uncheckedPaths = {"src/main/resources/img/settings/redCircle.png",
                 "src/main/resources/img/settings/yellowCircle.png",
                 "src/main/resources/img/settings/greenCircle.png",
                 "src/main/resources/img/settings/blackCircle.png",
@@ -84,22 +135,39 @@ public class SettingsView extends JWindow implements View {
                 "src/main/resources/img/settings/greenCircleChecked.png",
                 "src/main/resources/img/settings/blackCircleChecked.png",
                 "src/main/resources/img/settings/purpleCircleChecked.png"};
+        Color[] colors = {new Color(255, 0, 0),
+                new Color(250, 200, 28),
+                new Color(64, 208, 182),
+                new Color(0, 0, 0),
+                new Color(100, 98, 252)};
         ButtonGroup group = new ButtonGroup();
         for (int i = 0; i < 5; i++) {
-            JCheckBox btn = createColorBtn(Path.of(paths[i]));
-            group.add(btn);
-            panel.add(btn);
+            ColorBtn btn = new ColorBtn(colors[i]);
+            btn.setBackground(Colors.TOOLBAR_BACKGROUND);
             try {
                 btn.setSelectedIcon(new ImageIcon(ImageIO.read(new File(checkedPaths[i]))));
+                btn.setIcon(new ImageIcon(ImageIO.read(new File(uncheckedPaths[i]))));
             } catch (IOException e) {
-                logger.warn("Can't load selected color icon " + e.getMessage());
+                logger.warn("Can't load color icon " + e.getMessage());
             }
+            btn.addItemListener(new ItemListener() {
+                @Override
+                public void itemStateChanged(ItemEvent e) {
+                    if (e.getStateChange() == ItemEvent.SELECTED){
+                        Color color = btn.getBtnColor();
+                        preview.setForeground(new Color(color.getRed(), color.getGreen(), color.getBlue(),
+                                preview.getForeground().getAlpha()));
+                    }
+                }
+            });
+            group.add(btn);
+            panel.add(btn);
         }
         return panel;
     }
 
-    private JTextField createColorTitle() {
-        JTextField color = new JTextField("Color");
+    private JTextField createJTextField(String text) {
+        JTextField color = new JTextField(text);
         color.setFont(new Font("Helvetica Neue", Font.PLAIN, 14));
         color.setForeground(new Color(155, 153, 165));
         color.setEditable(false);
@@ -142,7 +210,6 @@ public class SettingsView extends JWindow implements View {
         closeBtn.setFocusPainted(false);
         closeBtn.setBorderPainted(false);
         closeBtn.setContentAreaFilled(false);
-        closeBtn.setBackground(Colors.TOOLBAR_BACKGROUND);
         closeBtn.setBorder(null);
         closeBtn.setHorizontalAlignment(SwingConstants.RIGHT);
         return closeBtn;
@@ -177,5 +244,17 @@ public class SettingsView extends JWindow implements View {
             logger.error("Can't create color circle " + e.getMessage());
         }
         return btn;
+    }
+
+    private class ColorBtn extends JCheckBox{
+        private final Color btnColor;
+
+        public ColorBtn(Color btnColor) {
+            this.btnColor = btnColor;
+        }
+
+        public Color getBtnColor() {
+            return btnColor;
+        }
     }
 }
